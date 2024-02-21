1bd09b8095c0ac58c7c60e9a72ae87a586086411
Andan <141715477+AdanCC21@users.noreply.github.com>
2024-02-21 10:07:35 -0800

    Creacion propia

--- Nodos/nodo1.c
+++ Nodos/nodo1.c
@@ -18,26 +18,44 @@ typedef struct datos{
 int main()
 {
     struct datos *list = NULL;
-    int op = 0;
+    int op = 0,ind=0;
+    char name[20],
+        app[20];
+    
     do
     {
         printf("Que accion quiere realizar?\n");
-        printf("1.-Inicio\t2.-Final\t3.-Posicion\t0.-Salir\n");
-        op=valid("Fuera de rango",0,3);
+        printf("1.-Inicio\t2.-Final\t3.-Posicion\t4.-Imprimir\t0.-Salir\n");
+        op=valid("Fuera de rango",0,4);
         
         switch(op)
         {
             case 1:
+            printf("Ingrese su nombre\n");
+                gets(name);
 
+                printf(" Ingrese su apellido\n");
+                gets(app);
+                ind++;
+                create_bin(&list,name,app,ind);
             break;
             case 2:
+            printf("Ingrese su nombre\n");
+                gets(name);
+
+                printf(" Ingrese su apellido\n");
+                gets(app);
+                ind++;
+                c_end(&list,name,app,ind);            
 
             break;
             case 3:
 
             break;
+            case 4:
+                print(list);
             case 0:
-
+            break;
         }
 
     } while (op!=0);
@@ -45,11 +63,58 @@ int main()
 
 }
 
-datos *create_bin(struct datos *nodo)
+void create_bin(struct datos **nodo,char name[],char app[],int ind)
 {
     datos* newnodo = (struct datos*)malloc(sizeof(datos));
-    newnodo->siguiete=NULL;
+    strcpy(newnodo->name,name);
+    strcpy(newnodo->app,app);
+    newnodo->id=ind;
+
+    newnodo->siguiete=*nodo;
     newnodo->anterior=NULL;
-    nodo->siguiete=newnodo;
-    return newnodo;
+
+    *nodo=newnodo;
+
 }
+
+void c_end(struct datos** nodo, char name[],char app[],int ind)
+{
+    datos* newnodo = (struct datos*)malloc(sizeof(datos));
+    strcpy(newnodo->name,name);
+    strcpy(newnodo->app,app);
+    newnodo->id=ind;
+
+    newnodo->siguiete=NULL;
+    if(*nodo ==NULL)
+    {
+        *nodo=newnodo;
+    }
+    else
+    {
+        struct datos *temp = *nodo;
+        while(temp->siguiete!=NULL)
+        {
+            temp=temp->siguiete;
+        }
+        temp->siguiete=newnodo;
+        newnodo->anterior=temp;
+        
+    }
+
+}
+
+void specific_position()
+{
+    
+}
+
+void print(struct datos* head)
+{
+    struct datos *temp = head;
+    while(temp !=NULL)
+    {
+        printf("%-10s | %-10s | %-10d\n",temp->name,temp->app,temp->id);
+        temp=temp->siguiete;
+    }
+}
+
